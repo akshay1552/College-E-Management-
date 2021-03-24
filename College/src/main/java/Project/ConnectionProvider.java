c788601fc8e2c188f37bb6bdd4386a7662e96ddb
aksha <aksha@AKSHAY.mshome.net>
2021-03-24 22:49:17 +0530

    First commit

---
+++ College/src/main/java/Project/ConnectionProvider.java
@@ -0,0 +1,17 @@
+package Project;
+import java.sql.*;
+public class ConnectionProvider {
+public static Connection getCon()
+{
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project1","root","");
+		return con;
+	}
+	catch(Exception e)
+	{
+		System.out.println(e);
+		return null;
+	}
+}
+}
